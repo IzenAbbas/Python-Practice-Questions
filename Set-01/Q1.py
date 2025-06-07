c45f0c42fc8fdeef3112ce9fc9a8a4a48428dc97
Izen Abbas <l230748@lhr.nu.edu.pk>
2025-06-08 00:49:00 +0500

    Add files via upload

---
+++ Set-01/Q1.py
@@ -0,0 +1,4 @@
+str1=["Data","Science", "Mentorship", "Program"]
+str2=["By","CampusX"]
+result = '-'.join(str1) +'sstarted-' + '-'.join(str2)
+print(result)
---
+++ Set-01/Q5.py
@@ -0,0 +1,6 @@
+p=float(input("Enter Principal Amount: "))
+r=float(input("Enter Rate of Interest: "))
+t=float(input("Enter Time in Years: "))
+
+result=p*(r/100)*t
+print("Simple Interest is: ", result)
