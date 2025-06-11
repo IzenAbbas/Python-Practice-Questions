35a1ca12c3d2df0f92cb0c06d29cebae63f0a94e
Izen Abbas <l230748@lhr.nu.edu.pk>
2025-06-11 15:27:21 +0500

    Delete NestedLoops-and-Strings directory

--- NestedLoops-and-Strings/Problem06.py
+++
@@ -1,8 +0,0 @@
-n=8
-x=int(input("Input x: "))
-sum=0
-
-for i in range(1,n):
-    sum+=(1/i)*(((x-1)/x)**i)
-
-print(f'Logarithm of {x} approximated to 7 terms is: {round(sum,2)}')
--- NestedLoops-and-Strings/Problem11.py
+++
@@ -1,7 +0,0 @@
-x=input('Enter a String: ')
-temp=x.split()
-result=''
-for i in range(0,len(temp)):
-    result+=temp[i][0]
-result=result.upper()
-print('Short Form:',result)
