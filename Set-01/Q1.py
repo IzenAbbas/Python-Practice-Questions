f091d3081372c254dc8a8c9532c468b0c536aecb
Izen Abbas <l230748@lhr.nu.edu.pk>
2025-06-11 14:30:41 +0500

    Delete Set-01 directory

--- Set-01/Q1.py
+++
@@ -1,4 +0,0 @@
-str1=["Data","Science", "Mentorship", "Program"]
-str2=["By","CampusX"]
-result = '-'.join(str1) +'sstarted-' + '-'.join(str2)
-print(result)
--- Set-01/Q5.py
+++
@@ -1,6 +0,0 @@
-p=float(input("Enter Principal Amount: "))
-r=float(input("Enter Rate of Interest: "))
-t=float(input("Enter Time in Years: "))
-
-result=p*(r/100)*t
-print("Simple Interest is: ", result)
